1ddf9a07312df3dcd6e7328ea2dc30bb358a89e2
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-15 00:04:00 +0530

    created serializer, urls and view for the blog and comment

--- api/views.py
+++ api/views.py
@@ -12,6 +12,9 @@ from django.http import Http404
 
 from rest_framework import mixins, generics, viewsets
 
+from blogs.models import Blog, Comment
+from blogs.serializers import BlogSerializer, CommentSerializer
+
 # Create your views here.
 # # Without serializer
 # def studentsView(request):
@@ -194,3 +197,24 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 # End Step 5 use view set with ModelViewSet
+
+
+
+######## Blogs app #########
+class BlogsView(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+class CommentsView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    lookup_field = 'pk'
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'pk'
